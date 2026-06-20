b2e658fc18c99e0fa6227a319d431e7259589242
Enzo Tang <enzet@andrew.cmu.edu>
2026-06-20 16:13:33 -0400

    Update gender_match.py

--- 3-gender_match/gender_match.py
+++ 3-gender_match/gender_match.py
@@ -1,1 +1,20 @@
+import pandas as pd
+
+INPUT = 'authors_clean.csv'
+CACHE = 'gender_cache.csv'
+OUTPUT = 'authors_gender.csv'
+
+authors = pd.read_csv(INPUT)         
+gender  = pd.read_csv(CACHE)        
+
+merged = authors.merge(
+    gender.rename(columns={'name': 'name_key'}),
+    on='name_key', how='left'
+)
+
+merged['gender_final'] = merged['gender'].fillna('unknown')
+
+out = merged[['hashed_author', 'first_name', 'name_key',
+              'gender', 'probability', 'count', 'gender_final']]
+out.to_csv(OUTPUT, index=False)
 
