3d8194bf98c065eb3718c22d1a542355960a7eae
Enzo Tang <enzet@andrew.cmu.edu>
2026-06-28 15:54:17 -0400

    Update new_panel.py

--- 4-gender panel/new_panel.py
+++ 4-gender panel/new_panel.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+PANEL  = 'panel.csv'
+GENDER = 'authors_gender_full.csv'
+OUTPUT = 'panel_gender_full.csv'
+
+panel  = pd.read_csv(PANEL)
+gender = pd.read_csv(GENDER)
+
+panel = panel.merge(gender, on='hashed_author', how='left')
+panel = panel[panel['gender_final'].isin(['male','female'])].copy()
+
+panel.to_csv(OUTPUT, index=False)
 
