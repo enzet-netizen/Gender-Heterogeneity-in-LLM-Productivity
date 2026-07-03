f6aec04936fb17d4f09c232793cab654c1b760de
Enzo Tang <enzet@andrew.cmu.edu>
2026-07-03 00:05:41 -0400

    Update stat.event_study.py

--- 5-regression/stat.event_study.py
+++ 5-regression/stat.event_study.py
@@ -1,34 +1,32 @@
 clear all
 import delimited "panel_gender_full.csv", clear
 
-ppmlhdfe monthly_productivity post_treated if gender_final == "male", absorb(hashed_author cohort_id##month_id) vce(cluster hashed_author)
-local b_m = _b[post_treated]
-local se_m = _se[post_treated]
-di "Male: coef=" `b_m' " se=" `se_m' " clusters=" e(N_clust)
+ppmlhdfe monthly_productivity rel_month_p* if gender_final == "male", absorb(hashed_author cohort_id##month_id rel_month) vce(cluster hashed_author)
+di "Male clusters: " e(N_clust) "  obs: " e(N)
 
-ppmlhdfe monthly_productivity post_treated if gender_final == "female", absorb(hashed_author cohort_id##month_id) vce(cluster hashed_author)
-local b_f = _b[post_treated]
-local se_f = _se[post_treated]
-di "Female: coef=" `b_f' " se=" `se_f' " clusters=" e(N_clust)
+matrix b = e(b)
+matrix V = e(V)
+local names : colnames b
+tempname mh
+postfile `mh' str40 coef double estimate double se using "coefs_male.dta", replace
+local k = 1
+foreach name of local names {
+    post `mh' ("`name'") (b[1, `k']) (sqrt(V[`k', `k']))
+    local k = `k' + 1
+}
+postclose `mh'
 
-ppmlhdfe monthly_productivity post_treated, absorb(hashed_author cohort_id##month_id) vce(cluster hashed_author)
-local b_all = _b[post_treated]
-local se_all = _se[post_treated]
-di "Overall: coef=" `b_all' " se=" `se_all' " clusters=" e(N_clust)
+ppmlhdfe monthly_productivity rel_month_p* if gender_final == "female", absorb(hashed_author cohort_id##month_id rel_month) vce(cluster hashed_author)
+di "Female clusters: " e(N_clust) "  obs: " e(N)
 
-clear
-set obs 3
-gen group = ""
-gen coef = .
-gen se = .
-replace group = "Overall" in 1
-replace coef = `b_all' in 1
-replace se = `se_all' in 1
-replace group = "Male" in 2
-replace coef = `b_m' in 2
-replace se = `se_m' in 2
-replace group = "Female" in 3
-replace coef = `b_f' in 3
-replace se = `se_f' in 3
-export delimited "gender_coefficients_full.csv", replace
-list
+matrix b = e(b)
+matrix V = e(V)
+local names : colnames b
+tempname mh
+postfile `mh' str40 coef double estimate double se using "coefs_female.dta", replace
+local k = 1
+foreach name of local names {
+    post `mh' ("`name'") (b[1, `k']) (sqrt(V[`k', `k']))
+    local k = `k' + 1
+}
+postclose `mh'
